533fd0df6874d368ea2442218cc342275066065f
Erik Lux <erik.lux@globallogic.com>
2016-02-08 10:32:13 +0100

    slices and strings updated

--- slices/slices.go
+++ slices/slices.go
@@ -34,15 +34,24 @@ func main() {
 	print_slice(s2)
 	print_slice(s3)
 	print_slice(s4)
-
+	fmt.Println("****************")
 	// Slice  is a reference on array with two values length and capacity
 	// to copy all use copy function
-	a2 := [...]int{1, 2, 3, 4, 5, 6, 7}
-	s5 := make([]int, 6)
-	n1 := copy(s5, a2[0:])
-	print_slice(s5)
-	fmt.Println(n1)
-	n2 := copy(s5, s5[2:])
-	print_slice(s5)
-	fmt.Println(n2)
+	/*
+	       a2 := [...]int{1, 2, 3, 4, 5, 6, 7}
+	   	s5 := make([]int, 6)
+	   	n1 := copy(s5, a2[0:])
+	   	print_slice(s5)
+	   	fmt.Println(n1)
+	   	n2 := copy(s5, s5[2:])
+	   	print_slice(s5)
+	   	fmt.Println(n2)
+	*/
+
+	head := 2
+	tail := 4
+	a2 := make([]int, 5)
+	copy(a2, a[head:tail])
+	print_slice(a2)
+
 }
--- strings1/strings1.go
+++ strings1/strings1.go
@@ -8,8 +8,6 @@ import (
 	"strings"
 )
 
-
-
 func main() {
 	r := bufio.NewReader(os.Stdin)
 
